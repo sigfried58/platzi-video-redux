5614346fea8c7b1b6c5cd2bdd8cf49ef8447554b
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-07-24 12:34:27 -0400

    Printing state data

--- src/entries/redux.js
+++ src/entries/redux.js
@@ -1,11 +1,42 @@
 // console.log('🙊');
+import { createStore, combineReducers } from 'redux';
 
 const handleSubmit = event => {
   event.preventDefault();
-  const data = new FormData($form);
+  const data = new FormData(form);
   const title = data.get('title');
   console.log(title);
 };
 
-const $form = document.getElementById('form');
-$form.addEventListener('submit', handleSubmit);
+const form = document.getElementById('form');
+form.addEventListener('submit', handleSubmit);
+
+const initialState = [
+  {
+    title: 'Despacito'
+  },
+  {
+    title: 'One more time'
+  },
+  {
+    title: 'Echame la culpa'
+  }
+];
+
+const store = createStore(
+  state => state,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+const container = document.getElementById('playlist');
+const playlist = store.getState();
+
+playlist.forEach(item => {
+  const template = document.createElement('p');
+  template.textContent = item.title;
+  // Añadir hijo
+  container.appendChild(template);
+});
+
+console.log(store.getState());
