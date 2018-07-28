ff1505065606792ad780e7222e21c8482277c77a
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-07-27 22:26:36 -0400

    Using standardized data in the playlist

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import Media from './media.js';
+// import Media from './media.js';
+import Media from '../containers/media.js';
 import './playlist.css';
 
 function Playlist(props) {
   return (
     <div className="Playlist">
-      {props.playlist.map(item => {
+      {props.playlist.map(mediaId => {
         return (
-          <Media openModal={props.handleOpenModal} {...item} key={item.id} />
+          <Media openModal={props.handleOpenModal} id={mediaId} key={mediaId} />
         );
       })}
     </div>
---
+++ src/playlist/containers/media.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Media from '../components/media';
+import { connect } from 'react-redux';
+
+class MediaContainer extends Component {
+  render() {
+    return <Media {...this.props.data} />;
+  }
+}
+
+const mapStateToProps = (state, props) => {
+  return {
+    data: state.data.entities.media[props.id]
+  };
+};
+
+export default connect(mapStateToProps)(MediaContainer);
