087361db172806352a696c858c48914ac9a41cf5
jimmy ibanez <jimmy.ibanez@outlook.com>
2018-08-06 15:17:19 -0400

    Action Types

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
+import {
+  OPEN_MODAL,
+  CLOSE_MODAL,
+  SEARCH_ENTITIES
+} from '../action-types/index';
+
 export const openModal = mediaId => {
   return {
-    type: 'OPEN_MODAL',
+    type: OPEN_MODAL,
     payload: {
       mediaId
     }
@@ -9,13 +15,13 @@ export const openModal = mediaId => {
 
 export const closeModal = () => {
   return {
-    type: 'CLOSE_MODAL'
+    type: CLOSE_MODAL
   };
 };
 
 export const searchEntities = query => {
   return {
-    type: 'SEARCH_ENTITIES',
+    type: SEARCH_ENTITIES,
     payload: {
       query
     }
--- src/reducers/data.js
+++ src/reducers/data.js
@@ -1,5 +1,6 @@
 import schema from '../schemas/index.js';
 import { fromJS } from 'immutable';
+import { SEARCH_ENTITIES } from '../action-types/index';
 
 const initialState = fromJS({
   entities: schema.entities,
@@ -9,7 +10,7 @@ const initialState = fromJS({
 
 const data = (state = initialState, action) => {
   switch (action.type) {
-    case 'SEARCH_ENTITIES': {
+    case SEARCH_ENTITIES: {
       // let results = [];
       // if (action.payload.query) {
       //   const categories = state.data.categories;
